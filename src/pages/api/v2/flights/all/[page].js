aea065979dcae599548b93df39789e58b78666e4
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2020-05-13 15:54:48 -0500

    clean out test code

--- src/pages/api/v2/flights/all/[page].js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-import url from 'url'
-
-export default (req: any, res: any ) => {
-    const { query: { page }} = req;
-    res.status(200).json({ page });
-}
--- src/pages/api/v2/info/bag-prices.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({ test: 'success' });
-};
