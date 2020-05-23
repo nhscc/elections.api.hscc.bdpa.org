6dcd92afbe84f0d7cdea50f8df7dae1af838c422
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2020-05-23 00:41:35 -0500

    void req

--- src/pages/api/v1/index.ts
+++ src/pages/api/v1/index.ts
@@ -4,6 +4,7 @@ import type { NextApiResponse } from 'next'
 import type { NextSessionRequest } from 'multiverse/simple-auth-session'
 
 export default async function(req: NextSessionRequest, res: NextApiResponse): Promise<void> {
+    void req;
     const client = await MongoClient.connect(process.env.MONGODB_URI || '<unknown>', { useUnifiedTopology: true });
     const db = client.db();
     const meta = db.collection('meta');
