7ddea03d0229d877a12721cf32f6e23ea8e17643
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2020-05-31 05:08:00 -0500

    updated directory structure

---
+++ src/pages/api/v1/election/[id]/index.ts
@@ -0,0 +1,12 @@
+import { handleEndpoint } from 'universe/backend/middleware'
+
+import type { NextApiResponse, NextApiRequest } from 'next'
+
+export { config } from 'universe/backend/middleware';
+
+export default async function(req: NextApiRequest, res: NextApiResponse) {
+    await handleEndpoint(async ({ res }) => {
+        // TODO...
+        void res;
+    }, { req, res, methods: [ 'GET', 'PUT', 'DELETE' ] });
+}
